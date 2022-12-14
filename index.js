f9ca239d3c328e30877973570d74a9c9fe6b9ad3
Marlen <02marlen95@gmail.com>
2022-12-14 15:02:55 -0600

    cors

---
+++ index.js
@@ -0,0 +1,32 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+app.use(cors({
+    origin: 'https://www.section.io'
+}));
+
+const ingredientes = [
+    {
+        "id": 1,
+        "item": "huevos"
+    },
+    {
+        "id": 2,
+        "item": "leche"
+    },
+    {
+        "id": 3,
+        "item": "hot-cakes"
+    },
+    {
+        "id": 4,
+        "item": "chilaquiles"
+    }
+];
+
+app.get('/ingredientes', (req, res) => {
+    res.send(ingredientes);
+});
+
+app.listen(3004);
